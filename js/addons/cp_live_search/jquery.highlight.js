a669730f1db20c1031fe49613307f6df216140b3
awaisqureshileyjao <awaisqureshi.leyjao@gmail.com>
2023-03-09 22:22:17 +0500

    testing

--- js/addons/cp_live_search/jquery.highlight.js
+++ js/addons/cp_live_search/jquery.highlight.js
@@ -109,14 +109,14 @@ jQuery.fn.highlight = function (words, options) {
 $(document).ready(function(){
     $("#search_input").click(function(){
         $(".ty-search-block").css({"position":"fixed","left":"0px","top":"0px","display":"block","width":"100%","borderRadius":"0px","zIndex":"1"});
-       /* $(".ty-search-block").dialog({
+        /*$(".ty-search-block").dialog({
             modal: true,
             resizable: true,
             dialogClass: 'no-close success-dialog'
         });
 
         $(".ui-dialog").css({"position":"fixed","left":"0px","top":"0px"});
-        $(".ui-widget-content").css({"width":"1000px","padding":"50px"});
-    });*/
+        $(".ui-widget-content").css({"width":"1000px","padding":"50px"});*/
+    });
 });
 
